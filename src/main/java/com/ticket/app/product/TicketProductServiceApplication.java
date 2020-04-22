3778abbf27d86a57f21b653855085851a0a3e597
jose <josealves_w@yahoo.com.br>
2020-04-22 02:31:00 -0300

    fist commit

---
+++ src/main/java/com/ticket/app/product/TicketProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.app.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketProductServiceApplication.class, args);
+	}
+
+}
